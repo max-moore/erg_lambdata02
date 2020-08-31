853396f7e306e754c34ac5b6bd1b513fa74cfb16
ilenzio <westfury@gmail.com>
2020-08-31 15:32:08 -0400

    trying to commit changes

--- lambdata/enzio_ml.py
+++ lambdata/enzio_ml.py
@@ -5,5 +5,5 @@ import pandas as pd
 
 def create_xmatrix_ytarget(df, target_string):
     y_target = df[target_string]
-    X_matrix = df.drop(["Total_Earnings"], axis=1)
+    X_matrix = df.drop(target_string, axis=1)
     return (X_matrix, y_target)
